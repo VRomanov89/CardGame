395cb713dcee71d9019a8cfa59e09a1d1ea99a8d
Vladimir Romanov <vladromweb@gmail.com>
2016-09-30 12:06:57 -0700

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Volodymyr Romanov on 9/30/16.
+//  Copyright © 2016 Vladimir Romanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Volodymyr Romanov on 9/30/16.
+//  Copyright © 2016 Vladimir Romanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
